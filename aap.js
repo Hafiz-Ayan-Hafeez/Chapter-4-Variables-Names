892acf49e76a41535d99603666075b5957909c29
Hafiz-Ayan-Hafeez <hafiz.ayan.public@gmail.com>
2024-09-15 23:36:58 +0500

    Chapter-4 Done

---
+++ aap.js
@@ -0,0 +1,32 @@
+var firstName , middleName , lastName;
+
+// 5 Legal Types Of Decleration Of Variables Are:-
+
+var name;
+var fullName;
+var $name;
+var _age;
+var age15;
+
+// 5 Illegal Types Of Decleration Of Variables Are:-
+
+// var -Name;
+// var full name;
+// var *username;
+// var 2name;
+// var 83;
+
+var head = document.querySelector("#head");
+head.innerHTML = "Rules For Naming JS Variables";
+
+var head_1 = document.querySelector("#head_1");
+head_1.innerHTML = "(1) Variables Name can only contain Numbers , $(Dollar) , _(Underscore) and Letters <br /> For Example:-$my_1stVariable";
+
+var head_2 = document.querySelector("#head_2");
+head_2.innerHTML = "(2) Variables must begin with a Letter , $(Dollar) , _(Underscore) <br /> For Example:-$name , _name or name";
+
+var head_3 = document.querySelector("#head_3");
+head_3.innerHTML = "(3) Variables Names Are Case Sensitive"; 
+
+var head_4 = document.querySelector("#head_4");
+head_4.innerHTML = "(4) Variables Names Should Not Be JS KeyWords";
